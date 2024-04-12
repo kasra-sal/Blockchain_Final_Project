f003b1d8fee24397474fb15f8332738d132d6959
Sam Saavedra <charmzjade@gmail.com>
2024-04-11 23:57:36 -0400

    Final Sam

--- Blockchain_Final.py
+++ Blockchain_Final.py
@@ -55,4 +55,38 @@ if __name__ == '__main__':
         blockchain.add_certificate(cert)
 
     
-   
+ 
+    while True:
+        print(" ########################################################\n",
+            "############ Block Chain Final Assignment ##############\n",
+            "############ Acedamic Document File System #############\n",
+            "########################################################\n")
+        print("Welcome to the Academic Document File System\n\n What would you like to do?\n\n 1. Access Documents\n\n 2. Store new document\n\n")
+        action = input("Enter your choice: ")
+
+        if action == "1":
+            
+            block_id = int(input("Please enter the ID of the document you'd like to access: "))
+            check_action = input("Would you like to\n1. Display certificate\n2. Verify Certificate\n3. Verify Digital Signature\n")
+            if check_action == '1':
+                print(blockchain.chain[int(block_id)])
+            elif check_action == '2':
+                cert_to_verify = certificates[block_id]
+                cert_validity = blockchain.verify_certificate(cert_to_verify)
+                print(f"\nCertificate '{cert_to_verify}' valid: {cert_validity}")
+            elif check_action == '3':
+                key_file = input("Please type the name of key file. (format: key.pem)\n")
+                with open('key.pem','rb') as f:
+                    data = f.read()
+                    public = RSA.import_key(data)
+                    if blockchain.verify_signature(blockchain.chain[block_id],public):
+                        print('Digital Signature is valid')
+                    else:
+                        print('file may be tampered with')
+
+
+        elif action == "2":
+            name = input("Please enter your name: ")
+            certificates.append("Certificate " + str(len(certificates)) + ": Issued to " + name)
+            print(certificates[-1], 'has been successfully added\n')
+            blockchain.add_certificate(certificates[-1])  
